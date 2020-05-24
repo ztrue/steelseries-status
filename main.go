a6ae7ce8271cfc26dee437a9c3a381e3dd19450b
ztrue <gztrue@gmail.com>
2020-05-24 12:49:14 -0700

    CLI arguments added

--- main.go
+++ main.go
@@ -4,8 +4,10 @@
 package main
 
 import (
+  "flag"
   "net/http"
   "os/exec"
+  "strings"
   "time"
 
   "github.com/spf13/afero"
@@ -14,24 +16,58 @@ import (
   "github.com/ztrue/steelseries-status/steelseries"
 )
 
-const GameName = "GO_TESTS"
+const GameName = "BUILD_STATUS"
 const DisplayName = "Build Status"
 const Developer = "github.com/ztrue"
 const EventPass = "PASS"
 
+type Config struct {
+  Command string
+  CorePropsPath string
+  Interval time.Duration
+}
+
 func main() {
-  if err := run(); err != nil {
+  if err := run(parseConfig()); err != nil {
     tracerr.PrintSourceColor(err)
   }
 }
 
-func run() error {
-  fs := afero.NewOsFs()
+func parseConfig() Config {
+  command := flag.String("c", "go test", "command to run")
+  corePropsPath := flag.String("p", "", "coreProps.json path")
+  intervalMS := flag.Int("t", 1000, "interval in ms")
 
-  d := steelseries.NewDiscoverer(fs, steelseries.CorePropsPathMacos)
-  props, err := d.CoreProps()
+  flag.Parse()
+
+  if *corePropsPath == "" {
+    // TODO Detect OS
+    *corePropsPath = steelseries.CorePropsPathMacos
+  }
+
+  return Config{
+    Command: *command,
+    CorePropsPath: *corePropsPath,
+    Interval: time.Duration(*intervalMS) * time.Millisecond,
+  }
+}
+
+func run(cfg Config) error {
+  ss, err := start(cfg)
   if err != nil {
     return tracerr.Wrap(err)
+  }
+
+  return process(ss, cfg)
+}
+
+func start(cfg Config) (*steelseries.Client, error) {
+  fs := afero.NewOsFs()
+
+  d := steelseries.NewDiscoverer(fs, cfg.CorePropsPath)
+  props, err := d.CoreProps()
+  if err != nil {
+    return nil, tracerr.Wrap(err)
   }
 
   httpClient := &http.Client{
@@ -42,16 +78,21 @@ func run() error {
 
   metadata := ss.BuildGameMetadata(DisplayName, Developer)
   if err := ss.SendGameMetadata(metadata); err != nil {
-    return tracerr.Wrap(err)
+    return nil, tracerr.Wrap(err)
   }
 
   event := ss.BuildBindGameEvent(EventPass)
   if err := ss.SendBindGameEvent(event); err != nil {
-    return tracerr.Wrap(err)
+    return nil, tracerr.Wrap(err)
   }
 
-  for range time.NewTicker(time.Second).C {
-    pass := exec.Command("go", "test", "./tests").Run() == nil
+  return ss, nil
+}
+
+func process(ss *steelseries.Client, cfg Config) error {
+  for range time.NewTicker(cfg.Interval).C {
+    segments := strings.Split(cfg.Command, " ")
+    pass := exec.Command(segments[0], segments[1:]...).Run() == nil
     value := 100
     if !pass {
       value = 0
@@ -61,6 +102,5 @@ func run() error {
       return tracerr.Wrap(err)
     }
   }
-
   return nil
 }
